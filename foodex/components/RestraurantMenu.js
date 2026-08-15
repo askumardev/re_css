53a6c8f7951a1a96d59f6da551bc75c57d0f5c16
askumardev <askumar.dev@outlook.com>
2026-08-15 18:36:01 +0530

    json

--- foodex/components/RestraurantMenu.js
+++ foodex/components/RestraurantMenu.js
@@ -5,9 +5,13 @@ const RestaurantMenu = () => {
   }, []);
 
   const fetchMenu = async () => {
-    const data = await fetch("https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.9351929&lng=77.62448069999999&restaurantId=425")
-    const json = await data.json();
+    const swiggyURL = "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.9351929&lng=77.62448069999999&restaurantId=425";
+    const proxiedUrl = `https://corsproxy.io/?url=${encodeURIComponent(swiggyURL)}`;
+    
+    const json = await fetch(proxiedUrl);
     console.log(json);
+    const text = await json.text();
+    console.log(text);
 
   };
   return (
