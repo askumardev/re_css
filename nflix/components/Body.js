a84ddd3e6d78f0f815d22604f70e9d9bedede7f1
askumardev <askumar.dev@outlook.com>
2026-08-28 15:37:39 +0530

    page setup

---
+++ nflix/components/Body.js
@@ -0,0 +1,25 @@
+import Login from "./Login.js";
+import Browse from "./Browse.js";
+import { createBrowserRouter, Link, RouterProvider } from "react-router-dom";
+
+
+const Body = () => {
+  const appRouter = createBrowserRouter([
+    {
+      path: "/",
+      element: <Login />,
+    },
+    {
+      path: "/browse",
+      element: <Browse />,
+    },
+
+  ]);
+  return (
+    <div>
+      <RouterProvider router={appRouter}/>
+    </div>
+  );
+};
+
+export default Body;
