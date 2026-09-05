0fb2b488015c2c041345ba497c7b02e7278f316a
askumardev <askumar.dev@outlook.com>
2026-09-05 05:00:07 +0000

    arrDesc

--- demo/demo.js
+++ demo/demo.js
@@ -3,14 +3,22 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import "./demo.css";
 
+function genRandomInt(max) {
+  return Math.floor(Math.random() * (max + 1));
+}
+
 function Header() {
+  const arrDesc  = ["Sample", "Core", "Crucial", 
+    "Important", "Essential", "Fundamental", "Necessary", 
+    "Required", "Vital", "Critical"];
+    const desc = arrDesc[genRandomInt(3)];
   return (
     <div>
       <header>
         <h1>React Essentials</h1>
         <img src="./atom.png" alt="React Atom Logo" />
         <p>
-          Fundamental React concepts you will need for almost any app you are
+          {desc} React concepts you will need for almost any app you are
           going to build!
         </p>
       </header>
